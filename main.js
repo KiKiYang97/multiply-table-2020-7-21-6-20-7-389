c9ad1c9ceacbafb23d7737a29c6fb78211e12fd9
KIKI YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGKI4@OOCL.COM>
2020-07-21 16:24:53 +0800

    createMultiplyTable

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 function createMultiplyTable(start, end) {
+  if(isNumbersValid(start,end)){
+    return generateMultiplyTable(start,end);
+  }
   return null;
 }
-
+function isNumbersValid(start,end){
+  var flag=true;
+  if(start>end){
+    flag = false;
+  }
+  if((start>1000||start<1)&&(end>1000||end<1)){
+    flag = false;
+  }
+  return flag;
+}
+function generateMultiplyTable(start,end){
+  var table="";
+  for(let i=start; i<=end;i++){
+    for(let j = start; j<=i; j++){
+      table += printMultiple(j,i);
+      if(j<i) table+="\t";
+    }
+    if(i<end)  table += "\n";
+  }
+  return table;
+}
+function printMultiple(multiplier,multiplicand){
+  var product = multiplier * multiplicand;
+  return multiplier + "*"+ multiplicand+"="+product;
+}
 module.exports = {
   createMultiplyTable,
 };
